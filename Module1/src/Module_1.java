2e72e547d00be95b6ecfd305744f6ce3fbe92e33
ruby -v <vikafire21@gmail.com>
2016-08-29 14:44:04 +0300

    Rejected commit:First code to this course

---
+++ Module1/src/Module_1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Vika on 29.08.2016.
+ */
+public class Module_1 {
+    static public void main (String[] args){
+        System.out.println("My First class to this course");
+    }
+}
