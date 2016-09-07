0e6534f6e5a6604036c950b2f9a28148ac0713ff
ruby -v <vikafire21@gmail.com>
2016-09-07 17:36:30 +0300

    Module 2 part 1

--- Modules/src/Module2Part2.java
+++ Modules/src/Module2Part2.java
@@ -4,22 +4,29 @@ public class Module2Part2 {
     public static void main(String[] args) {
 
         Double balance = 135.0;
-
+        Double commision;
         System.out.println("Please input the sum of money which you want withdraw from your account");
 
         Scanner sc = new Scanner(System.in);
         Double withdrawal = sc.nextDouble();
 
-        System.out.println(bank(balance, withdrawal));
-    }
-        static String bank(Double balance,Double withdrawal){
-            String bank = "No";
-            Double commision = balance / 100 * 5;
+        commision = withdrawal / 100 * 5;
 
-            if(balance > (withdrawal + commision)) {
-                balance = balance - withdrawal - commision;
-                bank = "OK " + commision + " " + balance;
-            }
-    return bank;
+
+        if (bank(balance, withdrawal, commision) == true ){
+            balance = balance - commision - withdrawal;
+            System.out.println("OK " + commision + " " + balance);
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+
+    static Boolean bank(Double balance,Double withdrawal,Double commision){
+        Boolean bank = true;
+        if (balance < (withdrawal + commision)){
+            bank = false;
+        }
+        return bank;
     }
 }
