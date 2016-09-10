d531af0f88b1d10d2557eacbc2ced24617620cb6
ruby -v <vikafire21@gmail.com>
2016-09-10 21:22:12 +0300

    Module 2 part 1

--- Modules/src/Module2Part3.java
+++ Modules/src/Module2Part3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Module2Part3 {
     public static void main(String[] args) {
 
-        int[] balances = {1200, 250, 2000,500, 3200};
+        int[] balances = {1200, 250, 2000, 500, 3200};
         String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
         System.out.println("Please enter you Name:");
@@ -13,12 +13,13 @@ public class Module2Part3 {
         System.out.println("Please enter the sum of money which you want withdraw from your account:");
         double withdrawal = sc.nextDouble();
 
-        if (bank(balances,ownerNames,ownerName,withdrawal)!=true){
+        if (bank(balances, ownerNames, ownerName, withdrawal) != true) {
             System.out.println("No");
         }
     }
+
     static Boolean bank(int[] balances, String[] ownerNames, String ownerName, double withdrawal) {
-        Boolean bank=true;
+        Boolean bank = true;
         int balanceInt = 0;
 
         for (int i = 0; i <= ownerNames.length - 1; i++) {
@@ -28,14 +29,15 @@ public class Module2Part3 {
                 double balance = (double) balanceInt;
                 double commision = withdrawal / 100 * 5;
 
-                if (balance > commision + withdrawal ){
+                if (balance > commision + withdrawal) {
                     balance = balance - commision - withdrawal;
-                   System.out.println("OK "+ withdrawal + " " + balance);
-            } else {
+                    System.out.println("OK " + withdrawal + " " + balance);
                     bank = false;
+                } else {
+                    bank = true;
                 }
-            }else {
-                bank = false;
+            } else {
+                bank = true;
             }
         }
         return bank;
