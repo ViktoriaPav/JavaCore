b7ac134a43f6b19f684adab36e687f6ec4354259
ruby -v <vikafire21@gmail.com>
2016-09-15 18:06:23 +0300

    Module 2 part 1

--- Modules/src/Module3/Module3Task2/Adder.java
+++ Modules/src/Module3/Module3Task2/Adder.java
@@ -2,10 +2,6 @@ package Module3.Module3Task2;
 
 public class Adder extends Arithmetic {
 
-    public Adder(Integer a, Integer b) {
-        super(a, b);
-    }
-
     public boolean check(Integer a, Integer b) {
         boolean check;
         if (a >= b) {
