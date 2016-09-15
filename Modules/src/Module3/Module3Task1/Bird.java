5d285cab3ff44920c32a196545dfa1114dc77a28
ruby -v <vikafire21@gmail.com>
2016-09-15 18:03:34 +0300

    Module 2 part 1

--- Modules/src/Module3/Module3Task1/Bird.java
+++ Modules/src/Module3/Module3Task1/Bird.java
@@ -2,10 +2,6 @@ package Module3.Module3Task1;
 
 public class Bird extends Animal {
 
-    public Bird(String breed, String name, int year, String place) {
-        super(breed, name, year, place);
-    }
-
     public void talking() {
         talk();
         System.out.println("I am flying");
