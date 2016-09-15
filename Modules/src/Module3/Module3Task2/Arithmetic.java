7cba0d0a00a681457d3d345e958b9981c9e3e4d4
ruby -v <vikafire21@gmail.com>
2016-09-15 17:44:32 +0300

    Module 2 part 1

--- Modules/src/Module3/Module3Task2/Arithmetic.java
+++ Modules/src/Module3/Module3Task2/Arithmetic.java
@@ -9,23 +9,7 @@ public class Arithmetic {
         this.b = b;
     }
 
-    public Integer getA() {
-        return a;
-    }
-
-    public void setA(Integer a) {
-        this.a = a;
-    }
-
-    public Integer getB() {
-        return b;
-    }
-
-    public void setB(Integer b) {
-        this.b = b;
-    }
-
-    public int add(Integer a, Integer b) {
+     public int add(Integer a, Integer b) {
 
         int sum = a + b;
 
