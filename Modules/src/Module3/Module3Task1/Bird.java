b9b0dbd6c2f7b7d672920ac8de8539f31abf2302
ruby -v <vikafire21@gmail.com>
2016-09-15 14:12:53 +0300

    Module 2 part 1

--- Modules/src/Module3/Module3Task1/Bird.java
+++ Modules/src/Module3/Module3Task1/Bird.java
@@ -7,6 +7,7 @@ public class Bird extends Animal {
     }
 
     public void talking() {
+        talk();
         System.out.println("I am flying");
         System.out.println("I am singing");
         System.out.println("I am Bird");
