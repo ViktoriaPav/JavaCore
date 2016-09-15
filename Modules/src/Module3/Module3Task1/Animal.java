aefe9b4723ff604de9f07e0b7b078f590b048933
ruby -v <vikafire21@gmail.com>
2016-09-15 18:02:50 +0300

    Module 2 part 1

--- Modules/src/Module3/Module3Task1/Animal.java
+++ Modules/src/Module3/Module3Task1/Animal.java
@@ -1,17 +1,6 @@
 package Module3.Module3Task1;
 
 public class Animal {
-    private String breed;
-    private String name;
-    private int year;
-    private String place;
-
-    public Animal(String breed, String name, int year, String place) {
-        this.breed = breed;
-        this.name = name;
-        this.year = year;
-        this.place = place;
-    }
 
     public void talk() {
         System.out.println("I am waking");
